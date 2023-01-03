6b468edfa6516ad9b29ccd5cc056f601cb71ccd7
ratchapon prangthong <ppete2547@gmail.com>
2023-01-03 16:19:20 +0700

    lab11_3 fish

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include<cmath>
+#include<string>
+using namespace std;
+
+int main(){
+    float count = 0;
+    float mean, SD,x = 0;
+    string textline;
+    ifstream source;
+	source.open("score.txt");
+    while(getline(source,textline)){
+        mean += atof(textline.c_str());
+        x = x+pow(atof(textline.c_str()),2);
+        count++;
+    }
+    
+	SD = sqrt((1/count)*x-(pow(mean/count,2)));
+	
+
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = "<< mean/count << endl;
+    cout << "Standard deviation = " << SD;
+}
