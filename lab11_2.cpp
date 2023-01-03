2bf4f8c71cfd3d0b434ccf7b984b63e710819a9c
ratchapon prangthong <ppete2547@gmail.com>
2023-01-03 16:19:01 +0700

    lab11_2 fish

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -6,12 +6,15 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
+	string textline;
+	dest << "-------------------- BOOM ---------------------" << endl;
+	while(getline(source,textline)){
+	dest << textline << endl;
+	}
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
+    dest << "-------------------- HA!! ---------------------";
     source.close();
     dest.close();
 	return 0;
